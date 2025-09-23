befdbd7ca79794890b8498bc0228d30fd97da2ec
luisreyes07 <lui.reyesl@duocuc.cl>
2025-09-23 16:17:00 -0300

    problema con gitghub y hacer commit

---
+++ proyecto/phyton/miweb/dataeasy/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('', views.index, name='index'),  # login
+    path('home/', views.home, name='home'),
+    path('perfil/', views.perfil, name='perfil'),
+    path('configuracion/', views.configuracion, name='configuracion'),
+    path('logout/', LogoutView.as_view(next_page='index'), name='logout'),
+]
