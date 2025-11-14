8db934e3539ca419a62a7bae076d2d9b9a18c62d
luisreyes07 <81238867+luisreyes07@users.noreply.github.com>
2025-11-14 12:13:50 -0300

    ?

--- dataeasy/urls.py
+++ dataeasy/urls.py
@@ -17,12 +17,17 @@ urlpatterns = [
     path('inventario/', views.lista_inventario, name='inventario_lista'),
     path('inventario/editar/<int:id_producto>/', views.editar_producto, name='inventario_editar'),
     path('inventario/eliminar/<int:id_producto>/', views.eliminar_producto, name='inventario_eliminar'),
+<<<<<<< Updated upstream
     path('inventario/exportar/', views.exportar_excel, name='exportar_excel'),
     
     # NUEVO: crear producto
     path('inventario/nuevo/', views.crear_producto, name='inventario_nuevo'),
 
     # --- Gestión de Usuarios ---
+=======
+
+    # --- Gestión de Usuarios ---vene
+>>>>>>> Stashed changes
     path('usuarios/', views.lista_usuarios, name='lista_usuarios'),
     path('usuarios/crear/', views.crear_usuario, name='usuario_crear'),
     path('usuarios/editar/<int:pk>/', views.editar_usuario, name='usuario_editar'),
