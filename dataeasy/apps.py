a0962a932275a414ae6e3b130daddf42ef9797ac
diegoxvega <127541258+diegoxvega@users.noreply.github.com>
2025-11-28 19:58:11 -0300

    Delete dataeasy directory

--- dataeasy/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class DataeasyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dataeasy'
-
-    def ready(self):
-        # Importa los signals para que se conecten cuando arranque Django
-        import dataeasy.signals
