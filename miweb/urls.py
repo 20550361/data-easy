3c62da6eff2a6779976ebab7d762c1389950f344
diegoxvega <127541258+diegoxvega@users.noreply.github.com>
2025-11-28 19:58:19 -0300

    Delete miweb directory

--- miweb/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    
-    # Conecta todas las URLs de tu app 'dataeasy' a la raíz del sitio
-    path('', include('dataeasy.urls')),
-]
